79a1be3a90acd1934b5b98ccdab6d2c1ae3a90c2
Oneiroi <oneiroi@fedoraproject.org>
2019-02-15 12:16:00 +0000

    I can english, honest ...

--- python/2.x/tcp_lock_challenge/lockpick.py
+++ python/2.x/tcp_lock_challenge/lockpick.py
@@ -6,7 +6,7 @@ __author__ == 'David Busby oneiroi@fedoraproject.org'
 This is for python 2.7.x, _if_ you opt for python 3 due to differences in socket handling
 a lot of this code will no longer function.
 
-PS this solution is disarmed, you'll need to fix some purposefull errors within ;-)
+PS this solution is disarmed, you'll need to fix some purposeful errors within ;-)
 
 """
 import socket
